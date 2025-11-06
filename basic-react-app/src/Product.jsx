2776973bf95ad0fd1245348380befe29ffa0d731
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-11-06 21:17:18 +0500

    the error i was getting wile rendering arrays and the objects, of the other elements down from it

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,13 +1,14 @@
 import "./Product.css";
 
 function Product({ tittle, price, features }) {
-  const list = features.map((feature) => <li>{feature}</li>);
+  // const list = features.map((feature) => <li>{feature}</li>);
 
   return (
     <div className="Product">
       <h3>{tittle}</h3>
       <p>The Price is going to be: {price}</p>
-      <p>{list}</p>
+      {/* <p>{list}</p> */}
+      <p>{features.a}</p>
     </div>
   );
 }
